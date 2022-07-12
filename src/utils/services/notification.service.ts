d78b675e359ad6c12dc43b4b45bbc85ca8137431
Victor Durand <v.durand@piman-consultants.fr>
2022-07-12 16:30:22 +0400

    fix

--- src/utils/services/notification.service.ts
+++ src/utils/services/notification.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import * as firebase from 'firebase-admin';
 import { DataMessagePayload } from "firebase-admin/lib/messaging/messaging-api";
+import { join } from "path";
 import { User } from "src/endpoints/users/entities/user.entity";
 const fs = require('fs');
 @Injectable()
 export class NotificationService {
     constructor() {
-        const firebaseCredentials = JSON.parse(fs.readFileSync("./fcm.json"));
+        const firebaseCredentials = JSON.parse(fs.readFileSync(join(process.cwd(),"fcm.json")));
         console.log(firebaseCredentials);
         firebase.initializeApp({
             credential: firebase.credential.cert(firebaseCredentials),
